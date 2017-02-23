50de59c903e570bdacb7bd56a394598d53ce9cdd
Daniel <ddtxra@gmail.com>
2017-02-23 17:40:59 +0100

    adding cache

--- src/main/java/dsoluti/seo/App.java
+++ src/main/java/dsoluti/seo/App.java
@@ -1,11 +1,7 @@
 package dsoluti.seo;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
@@ -17,41 +13,47 @@ import io.vertx.core.http.HttpServer;
  */
 public class App {
 
+	private static final String regex = "^(?!.*?(\\.js|\\.css|\\.xml|\\.less|\\.png|\\.jpg|\\.jpeg|\\.gif|\\.pdf|\\.doc|\\.txt|\\.ico|\\.rss|\\.zip|\\.mp3|\\.rar|\\.exe|\\.wmv|\\.doc|\\.avi|\\.ppt|\\.mpg|\\.mpeg|\\.tif|\\.wav|\\.mov|\\.psd|\\.ai|\\.xls|\\.mp4|\\.m4a|\\.swf|\\.dat|\\.dmg|\\.iso|\\.flv|\\.m4v|\\.torrent|\\.ttf|\\.woff))(.*)";
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+	private static final Logger START_REQUEST_LOGGER = LoggerFactory.getLogger("request-start-file");
+	private static final Logger END_REQUEST_LOGGER = LoggerFactory.getLogger("request-end-file");
+
 	public static void main(String[] args) {
 
 		VertxOptions vertxOptions = new VertxOptions();
-		vertxOptions.setMaxEventLoopExecuteTime(60000000000L); // 20 seconds
+		vertxOptions.setMaxEventLoopExecuteTime(60000000000L); // 60 seconds
 
 		HttpServer server = Vertx.vertx(vertxOptions).createHttpServer();
 
 		server.requestHandler(request -> {
-
-			String regex = "^(?!.*?(\\.js|\\.css|\\.xml|\\.less|\\.png|\\.jpg|\\.jpeg|\\.gif|\\.pdf|\\.doc|\\.txt|\\.ico|\\.rss|\\.zip|\\.mp3|\\.rar|\\.exe|\\.wmv|\\.doc|\\.avi|\\.ppt|\\.mpg|\\.mpeg|\\.tif|\\.wav|\\.mov|\\.psd|\\.ai|\\.xls|\\.mp4|\\.m4a|\\.swf|\\.dat|\\.dmg|\\.iso|\\.flv|\\.m4v|\\.torrent|\\.ttf|\\.woff))(.*)";
-
-			//String base = "https://www.nextprot.org";
+			
+			START_REQUEST_LOGGER.info(request.getHeader("USER_AGENT") + request.host());
+			 
+			LOGGER.info(request.absoluteURI());
+			// String base = "https://www.nextprot.org";
 			String base = "https://bed-search.nextprot.org";
 
-			String requestedUrl = base +  request.path();
-			
-			System.out.println("Asking for " + requestedUrl + " and path is " + request.path());
-			
-			if(requestedUrl.matches(regex)){
+			String requestedUrl = base + request.path();
 
-				System.out.println("Responding with selenium");
+			LOGGER.info("Asking for " + requestedUrl + " and path is " + request.path());
 
-				String url =  request.absoluteURI().replaceAll("http://localhost:8082", base);
+			if (requestedUrl.matches(regex)) {
+
+				LOGGER.info("Responding with selenium for " + request.absoluteURI());
+
+				String url = request.absoluteURI().replaceAll("http://localhost:8082", base);
 				String content = WebRemoteDriver.getContent(url);
 				request.response().putHeader("content-type", "text/html").end(content);
 
+			} else {
 
-			}else {
-
-				System.out.println("Responding with redirect" + requestedUrl);
+				LOGGER.info("Responding with redirect for " + request.absoluteURI());
 				request.response().putHeader("location", requestedUrl).setStatusCode(302).end();
 			}
-
-
-
+			
+			
+			END_REQUEST_LOGGER.info(request.host());
 
 		});
 
--- src/main/java/dsoluti/seo/WebRemoteDriver.java
+++ src/main/java/dsoluti/seo/WebRemoteDriver.java
@@ -1,10 +1,21 @@
 package dsoluti.seo;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.config.units.MemoryUnit;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -13,42 +24,49 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebRemoteDriver {
+
+	static Cache<String, byte[]> htmlCache;
 	
-	private static Map<String, String> cache = new HashMap<>();
+	static {
+		
+		CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+			    .with(CacheManagerBuilder.persistence("cache"))
+			    .withCache("htmlCache", CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class, byte[].class, ResourcePoolsBuilder.newResourcePoolsBuilder()
+			                .disk(100, MemoryUnit.GB, true)))
+			        .build(true);
+
+		htmlCache = cacheManager.getCache("htmlCache", String.class, byte[].class);
+	}
 	
 	public static String getContent(String requestedUrl){
 		
-		if(cache.containsKey(requestedUrl)){
-			return cache.get(requestedUrl);
+		if(htmlCache.containsKey(requestedUrl)){
+			return unzip(htmlCache.get(requestedUrl));
 		}
 		
 		WebDriver driver = null;
 		URL hubUrl = null;
-		System.err.println("Starting ");
 
 		try {
-			hubUrl = new URL("http://localhost:4444/wd/hub");
+			hubUrl = new URL("http://dockerdev.vital-it.ch:32768/wd/hub");
 		} catch (MalformedURLException e1) {
 			e1.printStackTrace();
 			System.exit(1);
 		}
 
 		driver = new RemoteWebDriver(hubUrl, DesiredCapabilities.chrome());
-
 		driver.get(requestedUrl);
-		sleep(100);
-
+		sleep(1000);
 		
 		try {
 			//Waits for active connections to finish
 			(new WebDriverWait(driver, 50, 1000)).until(new ExpectedCondition<Boolean>() {
 	            public Boolean apply(WebDriver d) {
-	        		Object o = ((JavascriptExecutor) d).executeScript("return jQuery.active;");
-	        		System.out.println(o.getClass());
-	        		System.err.println(o);
+	        		Object o = ((JavascriptExecutor) d).executeScript("return ((jQuery)? jQuery.active : 0)");
 	            	return o.equals(0L);
 	            }
 	        });
+			
 		}catch (org.openqa.selenium.TimeoutException timeout) {
 			System.err.println("Not finished ... after timeout !!! ");
 		}
@@ -57,21 +75,20 @@ public class WebRemoteDriver {
 	
 		System.err.println("Finished waiting");
 
-		sleep(1000);
+		sleep(2000);
 		
 		String content = driver.getPageSource();
 
 		String contentWithoutJs = content.replaceAll("<script(.|\n)*?</script>", "");
 		String contentWithoutJsAndHtmlImport = contentWithoutJs.replaceAll("<link rel=\"import\".*/>", "");
+		//String contentWithCorrectBase = contentWithoutJsAndHtmlImport.replaceAll("(<base.*?>)", "<base href=\"" + base + "\"/>");
 		
-		String jqueryScript = "\n<script src=\"https://code.jquery.com/jquery-3.1.1.min.js\"></script>";
 		String bootstrapScript = "\n<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>";
-		String highchartScript = "\n<script src=\"https://rawgit.com/calipho-sib/nextprot-ui/develop/vendor/scripts/highstock.src.js\"></script>";
-		String finalContent = contentWithoutJsAndHtmlImport + "\n" + highchartScript + jqueryScript + bootstrapScript;
+		String finalContent = contentWithoutJsAndHtmlImport + "\n" + bootstrapScript;
 		
 		driver.quit();
 
-		cache.put(requestedUrl, finalContent);
+		htmlCache.put(requestedUrl, zip(finalContent));
 		return finalContent;
 
 	}
@@ -84,5 +101,37 @@ public class WebRemoteDriver {
 		}
 	}
 	
+	private static byte[] zip(String stringToCompress){
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		GZIPOutputStream gzipOut;
+		try {
+			gzipOut = new GZIPOutputStream(baos);
+			ObjectOutputStream objectOut = new ObjectOutputStream(gzipOut);
+			objectOut.writeObject(stringToCompress);
+			objectOut.close();
+			return baos.toByteArray();
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static String unzip(byte[] bytes){
+		
+		ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+		try {
+			GZIPInputStream gzipIn = new GZIPInputStream(bais);
+			ObjectInputStream objectIn = new ObjectInputStream(gzipIn);
+			String myObj2 = (String) objectIn.readObject();
+	  	    objectIn.close();
+			return myObj2;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 
 }
