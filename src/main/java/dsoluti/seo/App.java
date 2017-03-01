c198f8aef926a2449b88a180c2baa0919ac5d868
Daniel <ddtxra@gmail.com>
2017-03-01 19:01:04 +0100

    adding logging

--- src/main/java/dsoluti/seo/App.java
+++ src/main/java/dsoluti/seo/App.java
@@ -1,14 +1,16 @@
 package dsoluti.seo;
 
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.LoggerFormat;
+import io.vertx.ext.web.handler.impl.LoggerHandlerImpl;
 
 /**
  * Simple HTTP Server that renders HTML pages using Selenium.
@@ -16,36 +18,38 @@ import io.vertx.core.http.HttpServer;
  */
 public class App {
 
-	//private static final String regex = "^(?!.*?(\\.js|\\.css|\\.xml|\\.less|\\.png|\\.jpg|\\.jpeg|\\.gif|\\.pdf|\\.doc|\\.txt|\\.ico|\\.rss|\\.zip|\\.mp3|\\.rar|\\.exe|\\.wmv|\\.doc|\\.avi|\\.ppt|\\.mpg|\\.mpeg|\\.tif|\\.wav|\\.mov|\\.psd|\\.ai|\\.xls|\\.mp4|\\.m4a|\\.swf|\\.dat|\\.dmg|\\.iso|\\.flv|\\.m4v|\\.torrent|\\.ttf|\\.woff))(.*)";
-
 	static final String regexToGetUrl = "http.\\/\\/.*?\\/(.*)";
 	static final String regexToGetDomain = "(http.:\\/\\/.*?\\/)";
 	static final Pattern patternToGetUrl = Pattern.compile(regexToGetUrl);
 	static final Pattern patternToGetDomain = Pattern.compile(regexToGetDomain);
-	
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
-	private static final Logger START_REQUEST_LOGGER = LoggerFactory.getLogger("request-start-file");
-	private static final Logger END_REQUEST_LOGGER = LoggerFactory.getLogger("request-end-file");
+    private static final Logger LOGGER = Logger.getLogger(App.class.getName());
 
 	public static void main(String[] args) {
 
 		VertxOptions vertxOptions = new VertxOptions();
 		vertxOptions.setMaxEventLoopExecuteTime(60000000000L); // 60 seconds
 
-		HttpServer server = Vertx.vertx(vertxOptions).createHttpServer();
+		Vertx vertx = Vertx.vertx(vertxOptions);
+		
+		HttpServer server = vertx.createHttpServer();
 
-		server.requestHandler(request -> {
+		Router router = Router.router(vertx);
 
-			START_REQUEST_LOGGER.info(request.getHeader("USER_AGENT") + request.host());
+		//By default, any blocking handlers executed on the same context (e.g. the same verticle instance) are ordered - this means the next one won’t be executed until the previous one has completed. If you don’t care about orderering and don’t mind your blocking handlers executing in parallel you can set the blocking handler specifying ordered as false using blockingHandler.
+		//We don't care in this case, therfore order = false
+		boolean ordered = false;
+		
+		router.route().path("/favicon.ico").blockingHandler(routingContext -> {
+			routingContext.response().setStatusCode(404).end();
+		});
+		
+		router.route().handler(new LoggerHandlerImpl(LoggerFormat.DEFAULT));
+		router.route().blockingHandler(routingContext -> {
 			
-			///http://localhost:8082/https://bed-search.nextprot.org/proteins/search?query=MSH6
-				
-			LOGGER.info(request.absoluteURI()); //         http://localhost:8082/https://bed-search.nextprot.org/proteins/search?query=MSH6
-			LOGGER.info(request.path()); //                /https://bed-search.nextprot.org/proteins/search
+			HttpServerRequest request = routingContext.request();
 			
 			String absoluteUrl = request.absoluteURI().replace("?_escaped_fragment_=", "");
-			LOGGER.info(absoluteUrl);
 			
 			if(request.path().contains("favicon.ico")){
 				request.response().setStatusCode(404).end();
@@ -59,25 +63,30 @@ public class App {
 				final Matcher domainMatcher = patternToGetDomain.matcher(requestedUrl);
 				String requestedDomain = null;
 				if (domainMatcher.find()) { requestedDomain = domainMatcher.group(1); }
+				
+				if(requestedUrl.length()<10){
+					request.response().setStatusCode(400).end("Request url is not valid: " + requestedUrl);
+				}else {
 
-				System.err.println("requestedDomain" + requestedDomain);
+					//TODO requestedDomain should be used here...
+					String content = WebRemoteDriver.getContent("https://bed-search.nextprot.org/", requestedUrl);
+					request.response().putHeader("content-type", "text/html").end(content);
 
-				LOGGER.info("Asking for " + requestedUrl + " and path is " + request.path());
+				}
 
-				System.err.println("REQUESTED URL" + requestedUrl);
-
-				String content = WebRemoteDriver.getContent("https://bed-search.nextprot.org/", requestedUrl);
-				request.response().putHeader("content-type", "text/html").end(content);
-
-				END_REQUEST_LOGGER.info(request.host());
 
 			}
+			
+			// Now call the next handler
+			//routingContext.next();
 
+		}, ordered);
+		
 
-		});
 
 		Integer port = 8082;
-		server.listen(port);
+		server.requestHandler(router::accept).listen(port);
+		
 		System.out.println("Listening to port " + port);
 
 	}
